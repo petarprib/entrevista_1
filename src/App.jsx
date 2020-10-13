04618221768e2cf642fc26df21ee47651f79002a
Petar Pribetić <pribetic.petar@gmail.com>
2020-10-13 12:31:34 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import products from './products.json';
+
+const App = () => {
+  return (
+    <div>
+      App.js
+    </div>
+  );
+}
+
+export default App;
